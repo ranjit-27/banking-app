2f4d6e5e3de981a7aaa651f80d4d69d0e4fa08ab
ranjit-27 <ranjithmadugula6@gmail.com>
2025-09-09 10:15:31 +0530

    Secure User module with password hashing ,JWt, input validation and error handling

---
+++ backend/src/graphql/schema.js
@@ -0,0 +1,17 @@
+import gql from "graphql-tag";
+import { userTypeDefs } from "./userTypeDefs.js";
+import { userResolvers } from "../resolvers/userResolvers.js";
+import { helloTypeDefs } from "./helloTypeDefs.js";
+import { helloResolver } from "../resolvers/helloResolver.js";
+
+const baseTypeDefs=gql`
+type Query{
+_empty:String
+}
+type Mutation{
+_empty:String
+}
+`;
+
+export const typeDefs=[baseTypeDefs,userTypeDefs,helloTypeDefs]
+export const resolvers=[userResolvers,helloResolver]
---
+++ backend/src/middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken"
+export const authMiddleWare=(req,res,next)=>{
+    const authHeader=req.headers.authorization;
+    const token=authHeader?.split(" ")[1]
+    if(token){
+        try{
+            const payload=jwt.verify(token,process.env.JWT_SECRET)
+            req.user=payload;
+        }catch(err){
+            req.user=null;
+        }
+    }else{
+        req.user=null
+    }
+
+    if(next){
+        next()
+    }
+}
